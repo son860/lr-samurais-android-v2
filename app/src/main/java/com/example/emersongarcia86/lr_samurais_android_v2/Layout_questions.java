0606448a9e4e8b4cfa8e99ea296ade3d30771048
Emerson Garcia <emersomggarcia86@gmail.com>
2016-11-10 22:41:43 -0200

    Novas tels no projeto

---
+++ app/src/main/java/com/example/emersongarcia86/lr_samurais_android_v2/Layout_questions.java
@@ -0,0 +1,13 @@
+package com.example.emersongarcia86.lr_samurais_android_v2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Layout_questions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_questions);
+    }
+}
