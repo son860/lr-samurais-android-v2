8f20eb3967e03830ddbb63ffd79ec9e9d58b6d65
Emerson <emerson.garcia@obxd.com.br>
2016-11-17 22:09:06 -0200

    Alteração do fragment questions

--- app/src/main/java/com/example/emersongarcia86/lr_samurais_android_v2/images_questions_fragment.java
+++ app/src/main/java/com/example/emersongarcia86/lr_samurais_android_v2/images_questions_fragment.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class images_groups_fragment extends Fragment {
+public class images_questions_fragment extends Fragment {
 
 
-    public images_groups_fragment() {
+    public images_questions_fragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +23,7 @@ public class images_groups_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_images_groups, container, false);
+        return inflater.inflate(R.layout.fragment_images_questions, container, false);
     }
 
 }
